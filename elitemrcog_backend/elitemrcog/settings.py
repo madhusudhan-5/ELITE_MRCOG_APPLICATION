95af736ec9ce55d3b872a604ba754dfc1295247f
madhusudhan-5 <tomcruisesmp007@gmail.com>
2026-09-05 14:43:39 +0530

    config(email): configure default from email to contact@elitemrcog.com

--- elitemrcog_backend/elitemrcog/settings.py
+++ elitemrcog_backend/elitemrcog/settings.py
@@ -142,7 +142,7 @@ EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=config('EMAIL_HOST_USER', default='noreply@elitemrcog.com'))
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=config('EMAIL_HOST_USER', default='contact@elitemrcog.com'))
 
 # ─── Summernote (Rich Text Editor) ────────────────────────────────────────────
 SUMMERNOTE_CONFIG = {
